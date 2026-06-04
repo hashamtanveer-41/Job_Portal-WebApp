f58ed9720407199f888c8f4575b97dadff72ac3a
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-04 15:32:12 +0500

    Add PostedJob component with tabs for active and draft jobs, and create PostedJobCard components

--- src/Pages/PostedJobPage.tsx
+++ src/Pages/PostedJobPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PostedJob from "../PostedJob/PostedJob";
 
 const PostedJobPage = () => {
     return (
         <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins'] px-4">
             <div className="flex gap-5 justify-between">
-
+                <PostedJob />
             </div>
 
         </div>
--- src/PostedJob/PostedJob.tsx
+++ src/PostedJob/PostedJob.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {Tabs, TabsList, TabsPanel} from "@mantine/core";
+import AboutCompany from "../CompanyProfile/AboutCompany";
+import CompanyJobs from "../CompanyProfile/CompanyJobs";
+import CompanyEmployee from "../CompanyProfile/CompanyEmployee";
+import {activeJobs} from "../../public/Data/PostedJob";
+import PostedJobCard from "./PostedJobCard";
 
 const PostedJob = () => {
     return (
-        <div>
+        <div className="w-1/6 mt-5">
+            <div className="text-2xl font-semibold mb-5 ">Jobs</div>
+            <div>
+                <Tabs autoContrast variant="pills" defaultValue="active">
+                    <TabsList className="[&_button[aria-selected='false']]:bg-mine-shaft-900 font-medium">
+                        <Tabs.Tab value="active">Active [4]</Tabs.Tab>
+                        <Tabs.Tab value="draft">Drafts [2]</Tabs.Tab>
+                    </TabsList>
+                    <TabsPanel value="active">
+                        <div className="flex flex-col gap-5 mt-5">
+                            {
+                                activeJobs.map((job, index)=>(
+                                    <PostedJobCard key={index} {...job} />
+                                ))
+                            }
+                        </div>
+                    </TabsPanel>
+                    <TabsPanel value="draft">
 
+                    </TabsPanel>
+                </Tabs>
+            </div>
         </div>
     )
 }
