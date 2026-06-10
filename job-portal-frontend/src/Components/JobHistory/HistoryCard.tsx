88cab7b766da97f46a7716bf52296c7e0cd27560
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-11 00:29:42 +0500

    Enhance HistoryCard and JobHistory components; implement save/unsave job functionality and improve job filtering

--- job-portal-frontend/src/Components/JobHistory/HistoryCard.tsx
+++ job-portal-frontend/src/Components/JobHistory/HistoryCard.tsx
@@ -2,17 +2,43 @@ import React from 'react'
 import {IconBookmark, IconBookmarkFilled, IconCalendarMonth, IconClockHour3} from "@tabler/icons-react";
 import {Button, Divider, Text} from "@mantine/core";
 import {Link} from "react-router-dom";
+import {timeAgo} from "../../Utils/Utilities";
+import {useDispatch, useSelector} from "react-redux";
+import {updateProfile} from "../../Store/action";
 
 const HistoryCard = (props:any) => {
+    const {profile} =useSelector((state:any) => state.profile)
+    const dispatch = useDispatch();
+    const handleSaveJob = () =>{
+        let savedJobs: any[] = Array.isArray(profile.savedJobs) ? [...profile.savedJobs] : [];
+        if (savedJobs?.includes(props.id)){
+            savedJobs=savedJobs?.filter((id:any)=>id!=props.id)
+        }else{
+            savedJobs=[...savedJobs, props.id];
+        }
+        let  updatedProfile:any = {...profile, savedJobs: savedJobs};
+        (dispatch as any)(updateProfile(updatedProfile, "Job saved successfully!"))
+    }
+    const handleUnSaveJob=()=>{
+        let savedJobs: any[] = Array.isArray(profile.savedJobs) ? [...profile.savedJobs] : [];
+        if (savedJobs?.includes(props.id)){
+            savedJobs=savedJobs?.filter((id:any)=>id!=props.id)
+        }else{
+            savedJobs=[...savedJobs, props.id];
+        }
+        let  updatedProfile:any = {...profile, savedJobs: savedJobs};
+        (dispatch as any)(updateProfile(updatedProfile, "Job unsaved successfully!"))
+
+    }
     return (
-        <Link to="/jobs" className="bg-mine-shaft-900 p-4 w-72 flex flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400">
+        <div  className="bg-mine-shaft-900 p-4 w-72 flex flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400">
             <div className="flex justify-between">
                 <div className="flex gap-2 items-center">
                     <div className="p-2 bg-mine-shaft-800 rounded-md">
                         <img className="h-7" src={`/Icons/${props.company}.png`} alt='Microsoft' /></div>
                     <div>
                         <div className="font-semibold">{props.jobTitle}</div>
-                        <div className="text-xs text-mine-shaft-300">{props.company} &#x022; {props.applicants} Applicants</div>
+                        <div className="text-xs text-mine-shaft-300">{props.company} &#x022; {props.applicants?props.applicants.length:0} Applicants</div>
                     </div>
                 </div>
                 <div>
@@ -20,7 +46,10 @@ const HistoryCard = (props:any) => {
                         props.saved?
                             <IconBookmarkFilled className="text-bright-sun-400 cursor-pointer"/>
                             :
-                            <IconBookmark className="text-mine-shaft-300 cursor-pointer"/>
+                            profile.savedJobs?.includes(props.id)?
+                                    <IconBookmarkFilled onClick={handleUnSaveJob} className="text-bright-sun-400 cursor-pointer hover:text-bright-sun-400"/>
+                                    :
+                                    <IconBookmark onClick={handleSaveJob} className="text-mine-shaft-300 cursor-pointer hover:text-bright-sun-400"/>
                     }
                 </div>
             </div>
@@ -30,12 +59,12 @@ const HistoryCard = (props:any) => {
                 <div>{props.location}</div>
             </div>
             <Text className="!text-xs text-justify !text-mine-shaft-300" lineClamp={3}>
-                {props.description}
+                {props.about}
             </Text>
             <Divider color='mine-shaft.7'  size="xs" />
             <div className="flex justify-between">
                 <div className="font-semibold text-mine-shaft-200">
-                    &#8360; {props.package}
+                    &#8360; {props.packageOffered} LPA
                 </div>
                 <div className="flex gap-1 text-mine-shaft-400 text-xs items-center">
                     <IconClockHour3 className="w-5 h-5" stroke={1.5}/>
@@ -48,7 +77,7 @@ const HistoryCard = (props:any) => {
                                 :
                                 "Posted"
                     }
-                    {props.postedDaysAgo} days ago
+                     {timeAgo(props.postTime)}
                 </div>
             </div>
             {(props.offered || props.interviewing) && <Divider color='mine-shaft.7'  size="xs" />}
@@ -65,7 +94,10 @@ const HistoryCard = (props:any) => {
                     <IconCalendarMonth stroke={1.5} className=" text-bright-sun-400 w-5 h-5"/> Sun, 25 Aug &bull; <span className="text-mine-shaft-400">10:00 AM</span>
                 </div>
             }
-        </Link>
+            <Link to={`/jobs/${props.id}`}>
+                <Button fullWidth color="brightSun.4" variant="outline">View Job</Button>
+            </Link>
+        </div>
     )
 }
 export default HistoryCard
--- job-portal-frontend/src/Components/JobHistory/JobHistory.tsx
+++ job-portal-frontend/src/Components/JobHistory/JobHistory.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Tabs, TabsList, TabsPanel} from "@mantine/core";
 import AboutCompany from "../CompanyProfile/AboutCompany";
 import CompanyJobs from "../CompanyProfile/CompanyJobs";
@@ -6,55 +6,53 @@ import CompanyEmployee from "../CompanyProfile/CompanyEmployee";
 import {jobList} from "../../../public/Data/JobsData";
 import JobCard from "../FindJobs/JobCard";
 import HistoryCard from "./HistoryCard";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllJobs} from "../../Store/action";
 
 const JobHistory = () => {
+    const [activeTab, setActiveTab] = useState<any>("APPLIED")
+    const [jobList, setJobList] = useState<any>([])
+    const [showList, setShowList] = useState<any>([])
+    const {profile}= useSelector((state:any) => state.profile)
+    const {user}= useSelector((state:any) => state.auth)
+    const dispatch = useDispatch();
+    useEffect(() => {
+        (dispatch as any)(getAllJobs(setJobList, setShowList, user));
+    }, []);
+    const handleTabChange= (value:string | null)=> {
+        setActiveTab(value)
+        console.log(value)
+        if (value==="SAVED"){
+            setShowList(jobList.filter((job:any)=> profile.savedJobs?.includes(job.id)));
+        }else{
+            setShowList(jobList.filter((job: any) =>
+                job.applicants?.some((applicant: any) =>
+                    applicant.applicantId == user.id && applicant.applicationStatus === value
+                )
+            ));
+        }
+    }
     return (
         <div>
             <div className="text-2xl font-semibold mb-5 ">Job History</div>
             <div>
-                <Tabs variant="outline" radius="lg" defaultValue="applied">
+                <Tabs value={activeTab} onChange={handleTabChange} variant="outline" radius="lg" defaultValue="applied">
                     <TabsList className="[&_button]:text-lg mb-5 font-semibold [&_button[data-active='true']]:text-bright-sun-400">
-                        <Tabs.Tab value="applied">Applied</Tabs.Tab>
-                        <Tabs.Tab value="saved">Saved</Tabs.Tab>
-                        <Tabs.Tab value="offered">Offered</Tabs.Tab>
-                        <Tabs.Tab value="interviewing">Interviewing</Tabs.Tab>
+                        <Tabs.Tab value="APPLIED">Applied</Tabs.Tab>
+                        <Tabs.Tab value="SAVED">Saved</Tabs.Tab>
+                        <Tabs.Tab value="OFFERED">Offered</Tabs.Tab>
+                        <Tabs.Tab value="INTERVIEWING">Interviewing</Tabs.Tab>
                     </TabsList>
-                    <TabsPanel value="applied">
-                        <div className="mt-10 flex flex-wrap gap-5 justify-between">
+                    <TabsPanel value={activeTab}>
+                        <div className="mt-10 flex flex-wrap gap-5 justify-items-start">
                             {
-                                jobList.map((job, index) => (
-                                    <HistoryCard {...job} key={index} applied/>
+                                showList.map((job:any, index:any) => (
+                                        <HistoryCard {...job} key={index} {...{[activeTab.toLowerCase()]:true}}/>
                                 ))
                             }
                         </div>
                     </TabsPanel>
-                    <TabsPanel value="saved">
-                        <div className="mt-10 flex flex-wrap gap-5 justify-between" >
-                        {
-                            jobList.map((job, index) => (
-                                <HistoryCard {...job} key={index} saved/>
-                            ))
-                        }
-                    </div>
-                    </TabsPanel>
-                    <TabsPanel value="offered">
-                        <div className="mt-10 flex flex-wrap gap-5 justify-between" >
-                        {
-                            jobList.map((job, index) => (
-                                <HistoryCard {...job} key={index} offered />
-                            ))
-                        }
-                    </div>
-                    </TabsPanel>
-                    <TabsPanel value="interviewing">
-                        <div className="mt-10 flex flex-wrap gap-5 justify-between">
-                            {
-                                jobList.map((job, index) => (
-                                    <HistoryCard {...job} key={index} interviewing />
-                                ))
-                            }
-                        </div>
-                    </TabsPanel>
+
                 </Tabs>
             </div>
         </div>
