ded4dfe5e3bcf7f9a94f909a412bd2f9cecca484
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-06-13 22:30:45 +0500

    Implement authentication functionality; add AuthConfig, AuthController, and update AuthenticationRequest for JWT login

---
+++ job-portal-backend/src/main/java/com/jobportalbackend/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.jobportalbackend.controller;
+
+import com.jobportalbackend.security.AuthenticationRequest;
+import com.jobportalbackend.security.AuthenticationResponse;
+import com.jobportalbackend.security.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getEmail());
+        final String jwt = jwtUtils.generateToken(userDetails.getUsername());
+        return new ResponseEntity<>(new AuthenticationResponse(jwt), HttpStatus.OK);
+    }
+}
